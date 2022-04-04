e06e09ce1f4c8ebb0bebcde7fe1c5ed066abd51e
Alexey Kuznetsov <42695162+paltusplintus@users.noreply.github.com>
2022-04-04 08:34:25 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import json
+import re
 import streamlit as st
 from neointerface import NeoInterface
 from pathlib import Path
@@ -11,63 +12,77 @@ neo.clean_slate()
 f = open(APP_JSON, "r")
 neo.load_arrows_dict(json.load(f))
 
+
+class App:
+    def __init__(self, interface: NeoInterface):
+        self.interface = interface
+        self.app_meta = self.fetch_metadata("App", {"id": APP})
+        self.app_params = {}
+        self.screen = st.empty()
+        self.render(0)
+
+    def fetch_metadata(self, label: str, where: dict = None):
+        if not where:
+            where = {}
+        q = f"""
+        MATCH path=(s:`{label}`)-[r]->()-[:NEXT*0..100]->(z)
+        WHERE NOT r:NEXT AND apoc.map.submap(apoc.map.merge(s, {{node_id: id(s)}}), keys($where)) = $where AND NOT (z)-[:NEXT]->()
+        WITH nodes(path)[1..] as coll
+        UNWIND coll as item
+        RETURN apoc.map.merge (item, {{node_id: id(item)}}) as item
+        """
+        params = {"where": where}
+        items = [res['item'] for res in self.interface.query(q, params)]
+        return items
+
+    def process_query(self, meta: dict):
+        if meta.get("query"):
+            q = meta.get("query")
+            if meta.get("params"):
+                params = json.loads(meta.get("params"))
+            else:
+                params = {}
+            params = {**params,
+                      **{id: widget for id, widget in self.app_params.items()}}
+            res = neo.query(q, params)
+            if res:
+                self.app_params = {**self.app_params, **res[-1]} # only last reslut of the query is written into params
+            return res
+
+    def process_param_str(self, s:str):
+        mod_s = s
+        for param in re.findall(r'\$(\w+?)\b', s):
+            if param in self.app_params.keys():
+                mod_s = mod_s.replace(f"${param}", self.app_params[param])
+        return mod_s
+
+    def render(self, ord: int):
+        for i, app_meta_item in enumerate(self.app_meta):
+            if i == ord:
+                screen_meta = self.fetch_metadata("Screen", {"node_id": app_meta_item['node_id']})
+                with self.screen.container():
+                    for j, widget_meta in enumerate(screen_meta):
+                        if widget_meta.get("type") == "Text":
+                            self.process_query(widget_meta)
+                            widget = st.write(self.process_param_str(widget_meta.get("label")))
+                        elif widget_meta.get("type") == "Input":
+                            widget = st.text_input(label="", value="", help=None)
+                        elif widget_meta.get("type") == "Button":
+                            widget = st.button(label=widget_meta.get("label"))
+                            actions = self.fetch_metadata("Widget", {"node_id": widget_meta['node_id']})
+                            if widget:
+                                for action in actions:
+                                    res = self.process_query(action)
+                                    if action.get("type") == "next screen":
+                                        st.session_state.active_screen += 1
+                                        self.render(st.session_state.active_screen)
+                        if widget_meta.get("id"):
+                            self.app_params[widget_meta.get("id")] = widget
+                    break
+            else:
+                self.screen.empty()
+
+
 if 'active_screen' not in st.session_state:
     st.session_state.active_screen = 0
-
-def fetch_metadata(interface: NeoInterface, label: str, where: dict = None):
-    if not where:
-        where = {}
-    q = f"""
-    MATCH path=(s:`{label}`)-[r]->()-[:NEXT*0..100]->(z)
-    WHERE NOT r:NEXT AND apoc.map.submap(apoc.map.merge(s, {{node_id: id(s)}}), keys($where)) = $where AND NOT (z)-[:NEXT]->()
-    WITH nodes(path)[1..] as coll
-    UNWIND coll as item
-    RETURN apoc.map.merge (item, {{node_id: id(item)}}) as item
-    """
-    params = {"where": where}
-    items = [res['item'] for res in interface.query(q, params)]
-    return items
-
-app_meta = fetch_metadata(neo, "App", {"id": APP})
-
-app_params = {}
-screen = st.empty()
-
-def process_query(meta:dict):
-    if meta.get("type") == "query":
-        q = meta.get("query")
-        if meta.get("params"):
-            params = json.loads(meta.get("params"))
-        else:
-            params = {}
-        params = {**params,
-                  **{id: widget for id, widget in app_params.items()}}
-        return neo.query(q, params)
-
-def render(ord: int):
-    for i, app_meta_item in enumerate(app_meta):
-        if i == ord:
-            screen_meta = fetch_metadata(neo, "Screen", {"node_id": app_meta_item['node_id']})
-            with screen.container():
-                for j, widget_meta in enumerate(screen_meta):
-                    if widget_meta.get("type") == "Text":
-                        widget = st.write(widget_meta.get("label"), key=(i * 1000 + j))
-                    elif widget_meta.get("type") == "Input":
-                        widget = st.text_input(label="", value="", help=None, key=(i * 1000 + j))
-                    elif widget_meta.get("type") == "Button":
-                        widget = st.button(label=widget_meta.get("label"), key=(i * 1000 + j))
-                        actions = fetch_metadata(neo, "Widget", {"node_id": widget_meta['node_id']})
-                        if widget:
-                            for action in actions:
-                                res = process_query(action)
-                                if action.get("type") == "next screen":
-                                    st.session_state.active_screen += 1
-                                    render(st.session_state.active_screen)
-                    if widget_meta.get("id"):
-                        app_params[widget_meta.get("id")] = widget
-                break
-        else:
-            screen.empty()
-
-
-render(st.session_state.active_screen)
+app = App(interface=neo)
